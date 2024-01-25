392697bc73cf8d502b6178d7d4a134c0a9ae468a
nivetharajendra <nivethar466@gmail.com>
2024-01-25 17:09:30 +0530

    one

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Service from './Service'
+
+export default function Home() {
+  return (
+    <div>
+    <Service/>
+    <Service/>
+    <Service/>
+    </div>
+  )
+}
---
+++ src/Pages/Service.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Pages/Service.css";
+export default function Service() {
+  return (
+    <div className='service'>
+      <div className='service-item'>01</div>
+      <div className='service-item'>02</div>
+      <div className='service-item'>03</div>
+      <div className='service-item'>04</div>
+    </div>
+  )
+}
